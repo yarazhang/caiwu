2be512bcede1c74350ae6f52d995291230498287
yarazhang <yarazhang0415@163.com>
2026-05-22 14:57:39 +0800

    Delete sync-index.ps1

--- sync-index.ps1
+++
@@ -1,3 +0,0 @@
-# 将主页面同步为 Vercel 首页 index.html
-Copy-Item -Path "$PSScriptRoot\other-fund-application.html" -Destination "$PSScriptRoot\index.html" -Force
-Write-Host "已同步: other-fund-application.html -> index.html"
